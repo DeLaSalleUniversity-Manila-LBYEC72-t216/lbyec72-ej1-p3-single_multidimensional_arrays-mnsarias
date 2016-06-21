dfe08b67a5358de77bf623fd15374fc6c96bd29f
mnsarias <pem_arias2@yahoo.com>
2016-06-21 17:24:23 +0800

    Update README.c

--- README.c
+++ README.c
@@ -1,2 +1,8 @@
 # lbyec72-ej1-p3-single_multidimensional_arrays-mnsarias
 lbyec72-ej1-p3-single_multidimensional_arrays-mnsarias created by GitHub Classroom
+
+![](main.jpg)
+![](poly1.jpg)
+![](stat1.jpg)
+![](matrix1.jpg)
+![](exit.jpg)
